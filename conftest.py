68853b0302f5ab1279d548d26d70a9bc3396d1d7
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-01-28 16:23:01 +0100

    komentarz nt. fixture i edycja docstringu metody metody api_request()

--- conftest.py
+++ conftest.py
@@ -1,12 +1,17 @@
 import pytest
 from playwright.sync_api import sync_playwright
 
+# @pytest.fixture(scope="session")
+# znak @ oznacza dekorator w Pythonie, który modyfikuje funkcję poniżej
+# scope="session" - tzn, że fixture będzie utworzony raz na sesję testową
+# tekst ograniczony """ (3 znakami ") to docstring funkcji, jej dokumentacja
 
 @pytest.fixture(scope="session")
 def api_request():
     """
     Wspólny kontekst APIRequestContext dla wszystkich testów (szybki).
-    ignore_https_errors=True ponieważ certyfikat SSL API może być nieaktualny.
+    ignore_https_errors=True - powód: certyfikat SSL API w przypadku tej usługi
+    może być nieaktualny.
     """
     base_url = "https://local-gov-units.polandapi.com"
 
