e6dd11b7df839ed48e1f32ba6b7e15a591a0efc4
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-01-29 15:09:02 +0100

    komentarz dot. testu przypadku negatywnego c.d.

--- tests/test_municipalities_by_name.py
+++ tests/test_municipalities_by_name.py
@@ -4,7 +4,8 @@ from tests.test_data import POSITIVE_CITIES, NEGATIVE_CITIES
 # Testy dla endpointu pobierającego jednostki samorządowe po nazwie miasta
 # Asercje sprawdzają, czy odpowiedzi API są zgodne z oczekiwaniami dla istniejących i nieistniejących nazw miast.
 # @pytest.mark.parametrize("city", POSITIVE_CITIES)
-# - to odpowiednik pętli for dla testów jednostkowych - uruchamia test dla każdej wartości w POSITIVE_CITIES
+# - odpowiednik pętli for dla testów jednostkowych 
+#   uruchamia test dla każdej wartości w POSITIVE_CITIES
 
 @pytest.mark.parametrize("city", POSITIVE_CITIES)
 def test_get_municipality_by_name_should_return_data(api_request, city):
