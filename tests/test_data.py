5dbf8a04d119acf1da24b8b3b14d28176b2692b8
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-02-15 17:07:18 +0100

    edycja danych testowych

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,12 @@
+# Dane testowe znajdują się w strukturach
+# w języku Python są to listy:
+# MIASTA_POSITIVE - zawiera poprawne nazwy miast
+# MIASTA_NEGATIVE - zawiera nieistniejące lub błędnie zapisan nazwy
+
 MIASTA_POSITIVE = [
+    "Katowice",
     "Swietochlowice",
-    "Kraków",
+    "Krakozja",
 ]
 
 MIASTA_NEGATIVE = [
